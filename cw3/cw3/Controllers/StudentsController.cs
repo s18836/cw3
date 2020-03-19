f0ccd48ccd66f1d8a2015772fb145c507387dab5
s18836 <s18836@pjwstk.edu.pl>
2020-03-19 09:56:18 +0100

    pierwszy commit

---
+++ cw3/cw3/Controllers/StudentsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cw3.Controllers
+{
+
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetStudent(string orderBy)
+        {
+            return $"Kowalski, Malewski, Andrzejewski sortowanie = {orderBy}";
+        }
+    }
+}
